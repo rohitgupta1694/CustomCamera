15ccb9def083e9885a90d97c053773d25da92315
errohitgupta <er.rohitgupta@outlook.com>
2016-02-13 01:12:36 +0530

    Camera Implementation

---
+++ dogethercamera/src/main/java/com/dogether/dogether/dogethercamera/CameraSettingPreferences.java
@@ -0,0 +1,38 @@
+package com.dogether.dogether.dogethercamera;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by dogether on 12/2/16.
+ */
+public class CameraSettingPreferences {
+
+    private static final String FLASH_MODE = "dogethercamera__flash_mode";
+
+    private static SharedPreferences getCameraSettingPreferences(@NonNull final Context context) {
+        return context.getSharedPreferences("com.dogether.dogether.dogethercamera", Context.MODE_PRIVATE);
+    }
+
+    protected static void saveCameraFlashMode(@NonNull final Context context, @NonNull final String cameraFlashMode) {
+        final SharedPreferences preferences = getCameraSettingPreferences(context);
+
+        if (preferences != null) {
+            final SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(FLASH_MODE, cameraFlashMode);
+            editor.apply();
+        }
+    }
+
+    protected static String getCameraFlashMode(@NonNull final Context context) {
+        final SharedPreferences preferences = getCameraSettingPreferences(context);
+
+        if (preferences != null) {
+            return preferences.getString(FLASH_MODE, Camera.Parameters.FLASH_MODE_AUTO);
+        }
+
+        return Camera.Parameters.FLASH_MODE_AUTO;
+    }
+}
---
+++ dogethercamera/src/main/java/com/dogether/dogether/dogethercamera/SquareImageView.java
@@ -0,0 +1,34 @@
+package com.dogether.dogether.dogethercamera;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by dogether on 12/2/16.
+ */
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        int squareLen = width > height ? height : width;
+        setMeasuredDimension(squareLen, squareLen);
+    }
+}
